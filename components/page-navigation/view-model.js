cf3e80b1518597f1d2a6a9a6c0a2a7dc83b145a4
santosh_giridhara <santosh_giridhara@apple.com>
2016-05-18 19:32:20 +0530

    Added page route and included grid component

--- components/page-navigation/view-model.js
+++ components/page-navigation/view-model.js
@@ -2,9 +2,6 @@ import can from 'can';
 import "can/route/pushstate/pushstate";
 import 'can/map/define/';
 import _ from 'lodash';
-import 'components/page-dashboard/';
-import 'components/page-samplegrid1/';
-import 'components/page-samplegrid2/';
 
 
 export default can.Map.extend({
@@ -13,17 +10,36 @@ export default can.Map.extend({
 	
 	gotoMenu1: function(){ 
 		console.log("gotoMenu1");
-		
+		//can.route.ready();
 		can.route.attr('page','dashboard');
+		Promise.all([
+	  		System.import('components/page-dashboard/')
+	    ]).then(function(results) {
+	    	var template = '<page-dashboard></page-dashboard>';
+      		$('#content').html(can.stache(template)());
+	    });		
+      	
 	},
 	gotoMenu2: function(){
 		console.log("gotoMenu2");
-		
+		//can.route.ready();
 		can.route.attr('page','samplegrid1');
+		Promise.all([
+	  		System.import('components/page-samplegrid1/')
+	    ]).then(function(results) {
+	    	var template = '<page-samplegrid1></page-samplegrid1>';
+      		$('#content').html(can.stache(template)());
+	    });
 	},
 	gotoMenu3: function(){
 		console.log("gotoMenu3");
-		
+		//can.route.ready();
 		can.route.attr('page','samplegrid2');
+		Promise.all([
+	  		System.import('components/page-samplegrid2/')
+	    ]).then(function(results) {
+	    	var template = '<page-samplegrid2></page-samplegrid2>';
+      		$('#content').html(can.stache(template)());
+	    });
 	}
 });
