92ad75fe50536f535766c1393447f5ff0217cf00
Santosh G Ksheersagar <Santosh G Ksheersagar>
2016-05-19 23:32:24 +0530

    Added responsive grid styles

--- components/page-navigation/page-navigation.js
+++ components/page-navigation/page-navigation.js
@@ -1,9 +1,11 @@
 import can from 'can';
 import Component from 'can/component/';
 import 'can/map/define/';
-import './style.less!';
+//import './style.less!';
+import './style-responsive.less!';
 import VM from './view-model';
-import template from './template.stache!';
+//import template from './template.stache!';
+import template from './template-responsive.stache!';
 import 'components/page-dashboard/';
 import 'components/page-samplegrid1/';
 import 'components/page-samplegrid2/';
@@ -17,10 +19,18 @@ can.Component.extend({
 	events: {
 		'inserted': function() {
 			console.log('loaded navigation');
-			$("#menu-toggle").click(function(e) {
-			        e.preventDefault();
-			        $("#wrapper").toggleClass("active");
-			});
+			$(document).ready(function () {
+  $('[data-toggle=offcanvas]').click(function () {
+    if ($('.sidebar-offcanvas').css('background-color') == 'rgb(255, 255, 255)') {
+	    $('.list-group-item').attr('tabindex', '-1');
+    } else {
+	    $('.list-group-item').attr('tabindex', '');
+    }
+    $('.row-offcanvas').toggleClass('active');
+    
+  });
+});
+
 		}
 	}
 });
