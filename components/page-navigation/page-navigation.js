0c75a3a04b52dcff549b287d3da7d41d1e585584
santosh_giridhara <santosh_giridhara@apple.com>
2016-05-20 16:26:07 +0530

    Updated navigation logic

--- components/page-navigation/page-navigation.js
+++ components/page-navigation/page-navigation.js
@@ -26,12 +26,14 @@ can.Component.extend({
 					$('#menu-toggle').removeClass('move-left').addClass('move-right');
 					$('#menu-toggle span').removeClass('glyphicon-chevron-left').addClass('glyphicon-chevron-right');
 					$('#wrappers li').css('left','175px');
+					$('#wrapper').css('padding-left','55px');
 					$('.menu_text').css('display','none');
 				} else {
 					$('#menu-toggle').removeClass('move-right').addClass('move-left');
 					$('#menu-toggle span').removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-left');
 					$('#wrappers li').css('left','0px');
 					$('.menu_text').css('display','block').css('float','left');
+					$('#wrapper').css('padding-left','225px');
 				}
 			});
 			// $("#menu-toggle").click(function(e) {
