50dd3dfed3214b3744e492539f5ddcf749c0e32f
santosh_giridhara <santosh_giridhara@apple.com>
2016-06-07 16:42:41 +0530

    Added can-validate as part of form validation

--- components/page-banks/add-customer/view-model.js
+++ components/page-banks/add-customer/view-model.js
@@ -4,15 +4,49 @@ import Bank from 'models/bank/';
 import Customer from 'models/customer/';
 import _ from 'lodash';
 
+import 'can-validate/can-validate';
+import 'can-validate/map/validate/';
+import 'can-validate/shims/validatejs.shim';
+
 export default can.Map.extend({
 
     define: {
-        newCustomer: {
-            value: {
-                name: '',
-                id: '',
-                email: ''
+        // newCustomer: {
+        //     value: {
+        //         name: '',
+        //         id: '',
+        //         email: ''
+        //     }
+        // },
+        myNum: {
+            value: 'santosh',
+            validate: {
+                required: true,
+                numericality: {
+                    greaterThan: 5,
+                    message: '^This is a custom message! Hi!'
+                }
+            }
+        },
+        isRequired: {
+            value: false,
+            type: 'boolean'
+        },
+        myVal: {
+            type: 'string',
+            validate: {
+                required: function() {
+                    return this.attr('isRequired') || false;
+                },
+                validateOnInit: true
             }
         }
+    },
+    addCustomer: function(ctx, $el) {
+        return false;
+    },
+    doValidate: function() {
+        console.log(this.validate());
+        this.validate();
     }
 });
